beb6226d5e0ac15d8e0b2355f4438753dc8d9599
karleljonte <karlnjuguna@gmail.com>
2026-02-26 13:53:15 +0300

    added new files

---
+++ lesson2a.js
@@ -0,0 +1,20 @@
+// Object data types:
+// An object in javascript is a data type that store data in form of key value pairs.
+
+let person = {
+    Name : "Karl Njuguna",
+    Age  : "70",
+    isRegistered : true
+};
+
+console.log("the details of the person are: ", person)
+
+
+console.log(person.Name)
+
+console.log(person["Age"])
+
+// # What is the difference between a dictionary in python, an object in javascript and a json data when dealing with APIs
+
+let cars = ["Benze", "Audi", "Dodge", "Prado"]
+console.log(cars[2]) 
